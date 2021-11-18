c125597f2244ab77d47719745bc9a5f69e052bcc
Marc <mail@lpcvoid.com>
2021-11-18 20:41:20 +0100

    * added some examples

---
+++ examples/daytime_client.cpp
@@ -0,0 +1,55 @@
+#include "../src/Client.hpp"
+#include "../src/Server.hpp"
+#include <csignal>
+#include <iostream>
+
+using namespace std::chrono_literals;
+
+void exit_handler(int s){
+  std::cout << "Goodbye!" << std::endl;
+  exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char** argv) {
+
+  signal(SIGINT, exit_handler);
+
+  uint16_t port = 13;
+  std::string time_host = "time-b-b.nist.gov";
+  if (argc == 3) {
+    time_host = argv[1];
+    port = std::atol(argv[2]);
+  }
+
+  std::cout << "Connecting to " << time_host << " on port " << port << std::endl;
+  netlib::client client;
+
+  std::error_condition client_create_res =
+      client.connect(time_host, port, netlib::AddressFamily::IPv4,
+                     netlib::AddressProtocol::TCP, 1000ms);
+
+  if (client_create_res) {
+    std::cerr << "Error connecting to host, error : " << client_create_res.message() << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  uint32_t error_count = 0;
+  while (true) {
+    auto daytime_res = client.recv(1024, 1000ms);
+    if (daytime_res.second) {
+      if (error_count++ == 3) {
+        std::cerr << "Attempted to read three times, all failed, exiting. Error: " <<
+            daytime_res.second.message() << std::endl;
+        exit(EXIT_FAILURE);
+      }
+      std::this_thread::sleep_for(1000ms);
+    } else {
+      std::string daytime_string(daytime_res.first.begin(), daytime_res.first.end());
+      std::cout << daytime_string << std::endl;
+
+      exit(EXIT_SUCCESS);
+    }
+
+  }
+
+}
---
+++ examples/echo_server.cpp
@@ -0,0 +1,52 @@
+#include "../src/Client.hpp"
+#include "../src/Server.hpp"
+#include "../src/endpoint_accessor.hpp"
+#include <csignal>
+#include <iostream>
+
+void exit_handler(int s){
+  std::cout << "Goodbye!" << std::endl;
+  exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char** argv) {
+
+  signal (SIGINT, exit_handler);
+
+  uint16_t port = 8888;
+  std::string bind_host = "localhost";
+  if (argc == 3) {
+    bind_host = argv[1];
+    port = std::atol(argv[2]);
+  }
+
+  std::cout << "Creating echo server on " << bind_host << ":" << port << std::endl;
+  std::cout << "CTRL+C shuts down the server." << std::endl;
+
+  netlib::server server;
+  server.register_callback_on_connect(
+      [&](netlib::client_endpoint endpoint) -> std::vector<uint8_t> {
+        std::string ip = netlib::endpoint_accessor::ip_to_string(endpoint.addr, endpoint.addr_len).value();
+        std::cout << "Client connected! IP: " << ip << std::endl;
+        return {};
+      });
+  server.register_callback_on_recv(
+      [&](netlib::client_endpoint endpoint,
+          const std::vector<uint8_t> &data) -> std::vector<uint8_t> {
+        std::cout << "Client sent some data, echoing it back!" << std::endl;
+        return data;
+      });
+  std::error_condition server_create_res = server.create(bind_host,
+                                                         static_cast<uint16_t>(port),
+                                                         netlib::AddressFamily::IPv4,
+                                                         netlib::AddressProtocol::TCP);
+  if (server_create_res) {
+    std::cerr << "Error initializing server: " << server_create_res.message() << std::endl;
+  }
+
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+
+}
+
