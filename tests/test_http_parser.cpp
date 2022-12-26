3df66db7987a1d8106821e93f869c530b6ae3c64
Marc <mail@lpcvoid.com>
2022-12-26 16:51:22 +0100

    Add http parsing test

--- tests/test_http_parser.cpp
+++ tests/test_http_parser.cpp
@@ -1,3 +1,30 @@
-//
-// Created by lpcvoid on 26.12.22.
-//
+#include "../doctest/doctest/doctest.h"
+#include "../src/http/http.hpp"
+#include "../src/netlib.hpp"
+#include <atomic>
+
+using namespace std::chrono_literals;
+extern uint16_t test_port;
+
+TEST_CASE("Test HTTP response parser")
+{
+    netlib::http::http_response resp;
+
+    std::string raw_response = "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/html; charset=UTF-8\r\n"
+    "Content-Length: 1256\r\n"
+    "\r\n\r\n"
+    "TEST";
+
+    CHECK_FALSE(resp.from_raw_response(raw_response));
+    CHECK_EQ(resp.version.first, 1);
+    CHECK_EQ(resp.version.second, 1);
+    CHECK_EQ(resp.response_code, 200);
+    CHECK_EQ(resp.headers.size(), 2);
+    CHECK_EQ(resp.headers[0].first, "Content-Type");
+    CHECK_EQ(resp.headers[1].first, "Content-Length");
+    CHECK_EQ(resp.headers[0].second, " text/html; charset=UTF-8");
+    CHECK_EQ(resp.headers[1].second, " 1256");
+    CHECK_EQ(resp.body, "TEST");
+
+}
